f9bec87cc72ba52d554f9b8d43cc34ffa4579936
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-09-22 23:46:54 +0600

    Class9

---
+++ JS/main.js
@@ -0,0 +1,75 @@
+// Programming Language
+//     1. Variable
+//     2. Data Type (string, number, boolean, array, obj)
+//     3. Conditional Statement (if else)
+//     4. Loop (for, while, do while, for each)
+//     5. function
+
+// Variable
+
+// 3 types of varibles
+//     1. var
+//     2. let
+//     3. const
+
+// var mostakVai45623 = Book;
+
+// variable declaration part
+// const variableName = 12;
+// value assign part
+// variableName = "String";
+
+// variableName = 1234;
+// variableName = true;
+
+// console.log(typeof variableName);
+// console.log(typeof variableName);
+// console.log(typeof variableName);
+
+// Data Types
+//      1. String ('' ba "" er moddhe ja thakbe tai string)
+//      2. number (i.e. 1234)
+//      3. boolean (true, false S.N: There won't be any quotation mark)
+//      4. Array (i.e. [1234, "Mango", true])
+//      5. Object (i.e. )
+
+// {
+// key: value, name: "Biplob", cgpa: 2.00, passed: false, numbers: [1, 2, 3, 4, 5], { key3: value3, key4: value4 }
+// }
+
+// if (condition) {
+// If 1st Condition is true
+// statement 1
+// } else if (condition) {
+// If 2nd Condition is true
+// statement 2
+// } else if (condition) {
+// If 3rd Condition is true
+// statement 3
+// } else if (condition) {
+// If 4th Condition is true
+// statement 4
+// } else if (condition) {
+// If 5th Condition is true
+// statement 5
+// } else {
+// If every Conditions are false
+// statement 6
+// }
+
+// switch (expression (i.e.a + b)) {
+//   case value1:
+// If the value of the expression matches value1
+// Statement 1
+// break;
+//   case value2:
+// If the value of the expression matches value2
+// Statement 2
+// break;
+//   case value3:
+// If the value of the expression matches value3
+// Statement 3
+// break;
+//   default:
+// By default Statement 4
+// }
