919e3e7660973648bb4579b39d9082c167413433
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-09-24 00:19:33 +0600

    class10

--- JS/main.js
+++ JS/main.js
@@ -16,7 +16,7 @@
 
 // variable declaration part
 // const variableName = 12;
-// value assign part
+// value assignment part
 // variableName = "String";
 
 // variableName = 1234;
@@ -73,3 +73,92 @@
 //   default:
 // By default Statement 4
 // }
+
+// Loops
+
+// 1. For Loop
+
+// The statements will run repeatedly till condition is true
+
+// Steps of loops
+
+// 1st step:- Variable Initialize (Declaration+Assignment);
+// 2nd step:- Condition;
+// If condition is true
+
+// 3rd step:- statements run
+// 4th step:- Increment/Decrement
+
+// else It will break the loop
+// Operand (a, b), Operator (+, =)
+// a + b = 10
+
+// Condition to Body to Inc/Dec to Condition to Body to Inc/Dec to...
+
+// for (let i = 0; i < 10; i++) {
+//   console.log("Hello World");
+// }
+
+// while (Condition) {}
+
+// let i = 0;
+
+// while (i < 10) {
+//   console.log(i + ". Hello World");
+//   i++;
+// }
+
+// for (;;) {
+//   console.log("Hello");
+// }
+
+// let i = 0;
+
+// do {
+//   console.log(i + ". Hello World");
+//   i++;
+// } while (i < 10);
+
+// let arr = [
+//   "Mango",
+//   "Banana",
+//   "Guava",
+//   "Pine-Apple",
+//   "Apple",
+//   "JackFruit",
+//   "Litchi",
+// ];
+// let i = 1;
+// arr.forEach((element) => {
+//   console.log(i + ". " + element);
+//   i++;
+// });
+
+// Function
+
+// There will be some set of instruction defined
+// There need to call/invoke (which is start for Juice Machine)
+
+// function functionName(Parameters){}
+
+// function fruitMix() {
+//   console.log("Hello World");
+//   console.log("Shohomot Vai");
+// }
+
+// fruitMix();
+// fruitMix();
+// fruitMix();
+
+// function sum(a, b, c) {
+//   console.log("The sum of " + a + " " + b + " " + c + " is = " + (a + b + c));
+// }
+
+// functionCall(Arguments)
+// sum(1, 2, 3);
+// sum(4, 7, 3);
+
+// Concatination
+// 1 + 1 = 11
+// Summation
+// 1 + 1 = 2
