b514d99ab05c9920e97d3fda940c1a7f55f66ca9
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-09-30 00:22:14 +0600

    class12

---
+++ BOM/main.js
@@ -0,0 +1,58 @@
+// const openBtn = document.querySelector("#open");
+// const closeBtn = document.querySelector("#close");
+// const move = document.querySelector("#move");
+// const resize = document.querySelector("#resize");
+
+// openBtn.addEventListener("click", windowOpen());
+// closeBtn.addEventListener("click", windowClose());
+// move.addEventListener("click", function windowMove() {
+//   console.log(move);
+//   window.moveTo(100, 50);
+// });
+// resize.addEventListener("click", function windowResize() {
+//   console.log(resize);
+//     alert("Hello World");
+//   window.resizeBy(200, 100);
+// });
+
+// function windowOpen() {
+//   console.log(openBtn);
+//   window.open("https://www.google.com");
+// }
+// function windowClose() {
+//   window.close();
+//   console.log(closeBtn);
+// }
+// function windowMove() {
+//   console.log(move);
+//   window.moveBy(100, 50);
+// }
+// function windowResize() {
+//   console.log(resize);
+//   alert("Hello World");
+//   window.resizeBy(200, 100);
+// }
+
+// const name = window.confirm("Enter your name...");
+// console.log(name);
+// let value = 1;
+// const counterText = document.querySelector("#counterText");
+// const counter = document.querySelector("#counter");
+// let v = 10;
+// console.log(counter);
+
+// const xyz = setInterval(function callBackFunction() {
+//   counterText.innerHTML = value;
+//   counter.style = `transform: translateX(${v}px)`;
+//   v += 1;
+
+//   if (value === 300) {
+//     clearTimeout(wxy);
+//     console.log("Clear Timeout Successfull");
+//   }
+//   value++;
+// }, 10);
+
+// const wxy = setTimeout(function callBackFunction() {
+//   console.log("Hello World");
+// }, 5000);
